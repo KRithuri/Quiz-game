14c47cff3fa542401b780db4e6a0bdbc53e4ab90
Karabo Rithuri <rithurikarabo@gmail.com>
2023-11-15 19:48:52 +0200

    question 1 - CPU

--- Quiz game/Quiz.py
+++ Quiz game/Quiz.py
@@ -1,2 +1,10 @@
 #function declaration
 def play_game():
+    #if answer is yes, continue with the game
+    print("Okay! Let's test your knowledge...")
+    score = 0
+
+    answer = input("What does CPU stand for? ")
+    if answer.lower() == "central  processing unit":
+        print("Correct!")
+        score += 1
