a872c6733826103df53b856a01ff0c8984635587
Karabo Rithuri <rithurikarabo@gmail.com>
2023-11-26 21:32:28 +0200

    function for when start btn is clicked

--- HTML, CSS, JS/quiz.js
+++ HTML, CSS, JS/quiz.js
@@ -1,2 +1,6 @@
 const startBtn = document.querySelector('.start-btn'); //fetch start button
 const popupInfo = document.querySelector('.popup-info') //fetch quiz guide 
+
+startBtn.onclick = () => {
+    popupInfo.classList.add('active');
+}
