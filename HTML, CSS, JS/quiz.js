86bb09d9fc4b7c07ba7504365892c69f8608fdb6
Karabo Rithuri <rithurikarabo@gmail.com>
2023-11-27 22:15:24 +0200

    fetch main class

--- HTML, CSS, JS/quiz.js
+++ HTML, CSS, JS/quiz.js
@@ -1,6 +1,7 @@
 const startBtn = document.querySelector('.start-btn'); //fetch start button
 const popupInfo = document.querySelector('.popup-info') //fetch quiz guide 
 const exitBtn = document.querySelector('.exit-btn'); //fetch exit button
+const main = document.querySelector('.main') //fetch main class
 
 //function to execute when start button is clicked
 startBtn.onclick = () => {
