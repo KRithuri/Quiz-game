b540463060c1391e6c7ade59dbdc5fc4399d47db
Karabo Rithuri <rithurikarabo@gmail.com>
2023-11-27 22:12:26 +0200

    fetch exit button

--- HTML, CSS, JS/quiz.js
+++ HTML, CSS, JS/quiz.js
@@ -1,6 +1,8 @@
 const startBtn = document.querySelector('.start-btn'); //fetch start button
 const popupInfo = document.querySelector('.popup-info') //fetch quiz guide 
+const exitBtn = document.querySelector('.exit-btn'); //fetch exit button
 
+//function to execute when start button is clicked
 startBtn.onclick = () => {
     popupInfo.classList.add('active');
 }
