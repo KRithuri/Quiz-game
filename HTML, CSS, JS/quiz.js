5c40b9c78ce4e9209cfb56320bbd1f6491c4366e
Karabo Rithuri <rithurikarabo@gmail.com>
2023-11-27 22:29:44 +0200

    fetch continue button

--- HTML, CSS, JS/quiz.js
+++ HTML, CSS, JS/quiz.js
@@ -1,14 +1,17 @@
 const startBtn = document.querySelector('.start-btn'); //fetch start button
 const popupInfo = document.querySelector('.popup-info') //fetch quiz guide 
 const exitBtn = document.querySelector('.exit-btn'); //fetch exit button
-const main = document.querySelector('.main') //fetch main class
+const main = document.querySelector('.main'); //fetch main class
+const continueBtn = document.querySelector('.continue-btn');  //fetch continue button
 
 //function to execute when start button is clicked
 startBtn.onclick = () => {
     popupInfo.classList.add('active');
+    main.classList.add('active');
 }
 
 //function to execute when exit button is clicked
 exitBtn.onclick = () => {
     popupInfo.classList.remove('active');
+    main.classList.remove('active');
 }
