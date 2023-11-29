be501e31fd89ff24bfd5dcfe25ac5ca2553805c2
Karabo Rithuri <rithurikarabo@gmail.com>
2023-11-29 22:41:49 +0200

    create array from get choice options

--- HTML, CSS, JS/game.js
+++ HTML, CSS, JS/game.js
@@ -1,1 +1,3 @@
 const question = document.getElementById("question"); //get question
+const choices = Array.from(document.getElementsByClassName("choice-text")); //get choice text and create array
+
