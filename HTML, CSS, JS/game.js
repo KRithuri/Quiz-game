7e7f7db8528a62aff1c4a7363c7b3ff023eefb5a
Karabo Rithuri <rithurikarabo@gmail.com>
2023-11-29 22:42:52 +0200

    declare empty array to store current questions

--- HTML, CSS, JS/game.js
+++ HTML, CSS, JS/game.js
@@ -1,3 +1,7 @@
 const question = document.getElementById("question"); //get question
-const choices = Array.from(document.getElementsByClassName("choice-text")); //get choice text and create array
+const choices = Array.from(document.getElementsByClassName("choice-text")); //get choice text and createlet currentQuestion = {};
+
+let currentQuestion = {}; //empty array
+console.log(choices);
+
 
