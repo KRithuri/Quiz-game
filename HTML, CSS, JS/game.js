718d76c1da92f3db5512fbfc0583241d38c7777a
Karabo Rithuri <rithurikarabo@gmail.com>
2023-11-29 22:44:01 +0200

    variable to store score counter

--- HTML, CSS, JS/game.js
+++ HTML, CSS, JS/game.js
@@ -2,6 +2,9 @@ const question = document.getElementById("question"); //get question
 const choices = Array.from(document.getElementsByClassName("choice-text")); //get choice text and createlet currentQuestion = {};
 
 let currentQuestion = {}; //empty array
-console.log(choices);
+let acceptingAnswers = true;
+let score = 0; //score variable
+
+
 
 
