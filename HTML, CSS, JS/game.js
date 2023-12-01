d599f9a62fb72cff697e1c2e32c2a3bd76200df9
Karabo Rithuri <rithurikarabo@gmail.com>
2023-12-01 23:43:18 +0200

    get question counter

--- HTML, CSS, JS/game.js
+++ HTML, CSS, JS/game.js
@@ -1,5 +1,6 @@
 const question = document.getElementById("question"); //get question
 const choices = Array.from(document.getElementsByClassName("choice-text")); //get choice text and createlet currentQuestion = {};
+const questionCounter = document.getElementById('questionCounter'); //get question counter
 
 let currentQuestion = {}; //empty array
 let acceptingAnswers = false;
