dcabdc6bc1f4c1043cffd6b798fa152b154aa115
Karabo Rithuri <rithurikarabo@gmail.com>
2023-12-02 14:03:06 +0200

    load and display high scores

--- HTML, CSS, JS/highscores.js
+++ HTML, CSS, JS/highscores.js
@@ -1,1 +1,7 @@
 const highScore = document.getElementById('highScoresList'); //fetch high scores list
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+highScoresList.innerHTML = highScores.map( score => {
+    return `<li class="high-score">${score.name} - ${score.score}</li>`;
+})
+.join("");
