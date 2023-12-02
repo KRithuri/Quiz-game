370f1efcaf570dffec458e069fbb2d702b75c9c9
Karabo Rithuri <rithurikarabo@gmail.com>
2023-12-02 16:14:21 +0200

    loader

--- HTML, CSS, JS/end.js
+++ HTML, CSS, JS/end.js
@@ -9,14 +9,13 @@ const MAX_HIGH_SCORES = 5;
 
 finalScore.innerText = mostRecentScore;
 
-username.addEventListener('keyup', () =>{
-    console.log(username.value);
+username.addEventListener("keyup", () => {
     saveScoreBtn.disabled = !username.value;
 })
 
 //function to save high score
 saveHighScore = (e) => {
-    console.log("Click the save button!");
+    console.log("Clickedd the save button!");
     e.preventDefault();
     
     const score = {
@@ -30,5 +29,5 @@ saveHighScore = (e) => {
     highScores.splice(5);
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('/');
+    window.location.assign('highscores.html');
 }
--- HTML, CSS, JS/game.js
+++ HTML, CSS, JS/game.js
@@ -3,6 +3,9 @@ const choices = Array.from(document.getElementsByClassName("choice-text")); //ge
 const progressText = document.getElementById('progressText'); //get question counter
 const scoreText = document.getElementById('score'); //fetch score 
 const progressBarFull = document.getElementById("progressBarFull"); //fetch progress bar
+const loader = document.getElementById('loader') //fetch loader
+const game = document.getElementById('game');
+
 
 let currentQuestion = {}; //empty array
 let acceptingAnswers = false;
@@ -11,49 +14,21 @@ let questionCounter = 0; //variable for question answer
 let availableQuestions = [];
 
 //array for questions and choices
-let questions = [
-    {
-        question: "What is the file extension for Python files? ",
-        choice1: ".python",
-        choice2: ".py",
-        choice3: "no extension",
-        choice4: ".pyt",
-        answer: 2
-    },
-    {
-        question: "How do you print 'Hello World!' to user in Python? ",
-        choice1: "console.log('Hello World!');",
-        choice2: "print('Hello World!')",
-        choice3: "msgBox('Hello World!');",
-        choice4: "console.writeLine('Hello World');",
-        answer: 2
-    },
-    {
-        question: "What is the correct syntax for referencing to an external script called 'xxx.js'?",
-        choice1: "<script href='xxx.js'>",
-        choice2: "<script name='xxx.js'>",
-        choice3: "<script src='xxx.js'>",
-        choice4: "<script file='xxx.js'>",
-        answer: 3
-    },
-    {
-        question: "Inside which HTML element do we the JavaScript? ",
-        choice1: "<script>",
-        choice2: "<javascript",
-        choice3: "<js>",
-        choice4: "<scripting",
-        answer: 1
-    },
-    {
-        question: "How do you write 'Hello World' in an alert box? ",
-        choice1: "msgBox('Hello World');",
-        choice2: "alertBox('Hello World');",
-        choice3: "alert('Hello World');",
-        choice4: "msg('Hello World);",
-        answer: 3
-    }
+let questions = [];
 
-]
+//fetch questions
+fetch("questions.json").then( res => {
+    return res.json();
+}).then(loadedQuestions => {
+    console.log(loadedQuestions);
+    questions = loadedQuestions;
+
+    //call function
+    startGame();
+})
+.catch( err => {
+    console.error(err);
+})
 
 //Constants
 const CORRECT_BONUS = 10;
@@ -65,7 +40,10 @@ startGame = () => {
     score = 0;
     availableQuestions = [...questions];
     console.log(availableQuestions);
-    getNewQuestion()
+    getNewQuestion();
+  
+    loader.classList.add("hidden");
+    game.classList.remove("hidden");
 }
 
 //funtion to get new question
@@ -73,7 +51,7 @@ getNewQuestion = () => {
     if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS){
         localStorage.setItem("mostRecentScore", score);
         //go to the end page
-        return window.location.assign('/end.html');
+        return window.location.assign("end.html");
     }
 
    
@@ -133,5 +111,3 @@ incrementScore = num => {
     scoreText.innerText = score;
 }
 
-//call function
-startGame();
