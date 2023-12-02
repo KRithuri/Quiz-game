9c8019fdfa8e74f793e51d3893d244c3fe63b4e9
Karabo Rithuri <rithurikarabo@gmail.com>
2023-12-02 11:31:37 +0200

    fetch saveScoreBtn

--- HTML, CSS, JS/end.js
+++ HTML, CSS, JS/end.js
@@ -1,4 +1,9 @@
 const username = document.getElementById('username'); //fetch username
+const saveScoreBtn = document.getElementById('saveScoreBtn');  //fetch saveScoreBtn
+
+username.addEventListener('keyup', () =>{
+    console.log(username.value);
+})
 
 //function to save high score
 saveHighScore = (e) => {
