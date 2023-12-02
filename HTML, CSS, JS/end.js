bedac7abcbb88a9a13cecb32b01097dcf6caaadf
Karabo Rithuri <rithurikarabo@gmail.com>
2023-12-02 11:39:05 +0200

    fetch final score

--- HTML, CSS, JS/end.js
+++ HTML, CSS, JS/end.js
@@ -1,6 +1,7 @@
 const username = document.getElementById('username'); //fetch username
 const saveScoreBtn = document.getElementById('saveScoreBtn');  //fetch saveScoreBtn
 const mostRecentScore = localStorage.getItem('mostRecentScore'); //fetch most recent score
+const finalScore = document.getElementById('finalScore'); //fetch final score
 
 username.addEventListener('keyup', () =>{
     console.log(username.value);
